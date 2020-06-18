2d1ead834fb74107b06c737537ab4e088d19f08c
AkiraSama <AkiraSama@users.noreply.github.com>
2020-06-18 06:23:36 +0000

    Add command suite for admission age filtering.

--- cogs/admit.py
+++ cogs/admit.py
@@ -7,6 +7,9 @@ from discord import (
 from discord.ext.commands import (
     Bot,
     Cog,
+    Context,
+    group,
+    has_guild_permissions,
 )
 
 
@@ -33,6 +36,67 @@ class AdmitCog(Cog, name=COG_NAME):
             for guild_id, age
             in (await self.redis.hgetall(self.AGES_KEY)).items()
         }
+
+    @group()
+    @has_guild_permissions(manage_guild=True)
+    async def admit(self, ctx: Context):
+        """Manage server admission."""
+
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @admit.group(name='age')
+    async def admit_age(self, ctx: Context):
+        """Manage admission age threshold."""
+
+        if ctx.invoked_subcommand is None:
+            await ctx.invoke(ctx.command.get_command('get'))
+
+    @admit_age.command(name='get')
+    async def admit_age_get(self, ctx: Context):
+        """Get admission age threshold."""
+
+        min_hours = self.age_thresholds.get(ctx.guild.id)
+
+        if not min_hours:
+            await ctx.send(
+                f"No admission age threshold set for {ctx.guild.name}."
+            )
+            return
+
+        await ctx.send(
+            f"Admission age threshold for {ctx.guild.name} "
+            f"is {self.age_thresholds.get(ctx.guild.id)} hours."
+        )
+
+    @admit_age.command(name='set')
+    async def admit_age_set(self, ctx: Context, hours: int):
+        """Set admission age threshold."""
+
+        if hours <= 0:
+            await ctx.invoke(
+                ctx.command.parent.get_command('remove'),
+            )
+            return
+
+        await self.redis.hset(self.AGES_KEY, ctx.guild.id, hours)
+        self.age_thresholds[ctx.guild.id] = hours
+
+        await ctx.send(
+            f"Successfully updated admission age threshold to {hours} hours."
+        )
+
+    @admit_age.command(
+        name='remove',
+        aliases=('reset', 'del', 'delete', 'disable'),
+    )
+    async def admit_age_reset(self, ctx: Context):
+        """Remove admission age threshold."""
+
+        await self.redis.hdel(self.AGES_KEY, ctx.guild.id)
+        self.age_thresholds.pop(ctx.guild.id, None)
+
+        await ctx.send("Sucessfully removed admission age threshold.")
 
     @Cog.listener()
     async def on_member_join(self, member: Member):
